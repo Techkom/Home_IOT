4135a0e773649a94b6f93e498bc60cc1177d6976
Techkom Asus Soma <techkomt20@gmail.com>
2020-11-07 11:35:34 +0700

    finish wifi

--- Tedmon_Air/Tedmon_Air.ino
+++ Tedmon_Air/Tedmon_Air.ino
@@ -1,17 +1,45 @@
+/* Library Initialization */
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
 /* Pin Initialization */
+// Ultrasonic
 const int ultPP = 7; // Ultrasonic Ping Pin
 const int ultEP = 6; // Ultrasonic Echo Pin
+// ADS1115
+Adafruit_ADS1115 ads(0x49);
 
 /* Variable Initalization */
+// Ultrasonic
 long duration, inches, cm;
+// Wifi
+const char  ssid          = "Techkom";
+const char  password      = "8283452008";
+// MQTT
+const char* mqttServer    = "IP":
+const int   mqttPort      = 0000;
+const char* mqttUser      = "User";
+const char* mqttPassword  = "Password";
+const char* client_ID     = "Tedmon";
+// Setting ESPClient
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
- Serial.begin(9600);
+ Serial.begin(115200);
+ // Ultrasonic
  pinMode(ultPP, OUTPUT);
  pinmode(ultEP, INPUT);
+ // ADS
+ ads.setGain(GAIN_ONE);
+ ads.begin();
 }
 
 void loop(){
+ wifi();
  ultrasonic();
 }
 
@@ -38,4 +66,20 @@ long MTI (long microseconds) {
 
 long MTC (long microseconds) {
  return microseconds / 29 / 2;
+}
+
+void wifi(){
+ WiFi.begin(ssid, password);
+ for (int k=0; k<=5; k++){
+  if(WiFi.status() != WL_CONNECTED){
+   delay(500);
+  } else if (WiFi.status() == WL_CONNECTED){
+   Serial.println("Connected");
+   Serial.print("IP Address: ");
+   Serial.println(WiFi.localIP());
+  } else (
+   Serial.println("Not Connected");
+  )
+  delay(500);
+ }
 }
